34f5fa50d79ced59d6da26639c83a3f570f907b7
Marta Hernandez <martahdez90@users.noreply.github.com>
2020-05-21 12:00:35 +0200

    professional

---
+++ professional.ts
@@ -0,0 +1,31 @@
+
+class Professional {
+    public name: string;
+    public age: number;
+    public genre: string;
+    public weight: string;
+    public height: number;
+    public hairColor: string;
+    public eyeColor: string;
+    public race: string;
+    public isRetired: string;
+    public nationality: string;
+    public oscarsNumber: number;
+    public profession: string;
+    //Implementación del método constructor
+    constructor(name: string, age: number, genre: string, weight: string;, height: number, hairColor: string, eyeColor: string, race: string, isRetired: string, nationality: string, oscarsNumber: number, profession: string) {
+        this.name = name;
+        this.age = age;
+        this.genre = genre;
+        this.weight = weight;
+        this.height = height;
+        this.hairColor = hairColor;
+        this.eyeColor = eyeColor;
+        this.race = race;
+        this.isRetired = isRetired;
+        this.nationality = nationality;
+        this.oscarsNumber = oscarsNumber;
+        this.profession = profession;
+    }
+
+}
