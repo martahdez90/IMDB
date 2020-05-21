f7e10d5e10c5826162e84be56cb74f0cec71fa8b
Marta Hernandez <65173504+martahdez90@users.noreply.github.com>
2020-05-21 12:36:00 +0200

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var professional_1 = require("./professional");
-var Professional1 = new professional_1.Professional("Julia Roberts", 40, "female", 60, 170, "brown", "blue", "white", "no", "USA", 2, "actress");
-var Professional2 = new professional_1.Professional("Marta Rodriguez", 20, "female", 65, 174, "yellow", "green", "white", "yes", "USA", 2, "actress");
-var Professional3 = new professional_1.Professional("Paul Williams", 30, "male", 80, 170, "brown", "blue", "white", "yes", "USA", 2, "actor");
-Professional1.print();
-Professional2.print();
-Professional3.print();
